c8c6732b5da5752011401283b4f3697b34a34eb5
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2021-12-15 12:20:19 +0900

    Update APP3.py

--- APP3.py
+++ APP3.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 import pandas as pd
 
@@ -10,17 +10,3 @@ def main():
     st.write(df.head())
 
 if __name__=='__main__':
-=======
-import streamlit as st
-import pandas as pd
-
-def main():
-    df=pd.read_csv('data/iris.csv')
-    st.dataframe(df)
-    species=df['species'].unique()
-    st.text('아이리스 꽃은' + species+'으로 되어있다.')
-    st.write(df.head())
-
-if __name__=='__main__':
->>>>>>> ec64fbbfdfbc15644b38cc90f56e3d671e0181b7
-    main()
